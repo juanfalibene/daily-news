ec497094debea0caae4b529940c21135cf69637c
juanfalibene <juanignaciofalibene@gmail.com>
2024-04-07 18:55:17 +0200

    Credential response with jwtDecode

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,27 +1,51 @@
-import React from "react";
+import { useState } from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GoogleLogin } from "@react-oauth/google";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+//import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const Homepage = () => {
+  const dispatch = useDispatch();
+
+  //const [isLoading, setIsLoading] = useState(false);
+
   const responseMessage = (response) => {
     console.log(response);
+    console.log("USUARIO LOGUEADO");
+    dispatch(setSignedIn(true));
+    const credential = response.credential;
+    const decoded = jwtDecode(credential);
+    console.log(decoded);
+    dispatch(setUserData(decoded));
   };
+
   const errorMessage = (error) => {
     console.log(error);
   };
+  const isSignedIn = useSelector(selectSignedIn);
+
   return (
-    <GoogleOAuthProvider clientId='866285505105-icla8fkqpc2ck9cj250cf4jclrhi3k6t.apps.googleusercontent.com'>
-      <div className='home__page'>
+    <div className='home__page' style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
         <div className='login__message'>
-          <h2>📰 Daily News for You</h2>
-          <p>
-            We provide Daily News for your morning and evening. Just sign up and
-            start reading some quality news.
-          </p>
-          <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+          <GoogleOAuthProvider
+            clientId='866285505105-icla8fkqpc2ck9cj250cf4jclrhi3k6t.apps.googleusercontent.com'
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+          >
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+          </GoogleOAuthProvider>
         </div>
-      </div>
-    </GoogleOAuthProvider>
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 
