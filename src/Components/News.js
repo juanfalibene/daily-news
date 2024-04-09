ffc45d0cb3118ada419b90eb093e5f954217e35c
juanfalibene <juanignaciofalibene@gmail.com>
2024-04-09 12:21:23 +0200

    styles updated

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,11 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUserInput, setBlogData } from "../features/userSlice";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
 
 const News = () => {
   const searchInput = useSelector(selectUserInput);
   const apikey = "b020e78d6c337c965e8282b57ee419a2";
-  const url = `https://gnews.io/api/v4/search?q=${searchInput}&lang=es&max=10&apikey=${apikey}`;
+  const url = `https://gnews.io/api/v4/search?q=${searchInput}&lang=es&max=20&apikey=${apikey}`;
   const dispatch = useDispatch();
   const [blogs, setBlogs] = useState();
 
@@ -27,30 +38,68 @@ const News = () => {
   }, [searchInput]);
 
   return (
-    <div className='news__page'>
-      {loading ? <p>loading...</p> : ""}
-      <div className='news'>
+    <Container>
+      {loading ? <CircularProgress /> : ""}
+      <Grid container spacing={2} mb={4} mt={1}>
         {blogs?.articles?.map((blog) => (
-          <a
-            key={blog.publishedAt}
-            className='news_article'
-            target='_blank'
-            href={blog.url}
-            rel='noreferrer'
-          >
-            <img src={blog.image} alt={blog.title} key={blog.image} />
-            <h3>{blog.title}</h3>
-            <h4>
-              <span>{blog.source.name}</span>
-              <span>{blog.publishedAt}</span>
-            </h4>
-            <p>{blog.description}</p>
-          </a>
+          <Grid item xs={12} md={4}>
+            <Card
+              sx={{ maxWidth: 600, backgroundColor: "rgba(0, 0, 0, 0.08)" }}
+              variant='outlined'
+            >
+              <CardMedia
+                sx={{
+                  width: "100%",
+                  height: 0,
+                  paddingBottom: "56.25%",
+                  backgroundColor: "rgba(0, 0, 0, 0.08)",
+                }}
+                image={blog.image}
+                alt={blog.title}
+                key={blog.image}
+                title={blog.title}
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant='h3'
+                  component='div'
+                  fontSize={18}
+                  fontWeight={400}
+                  color='text.primary'
+                >
+                  {blog.title}
+                </Typography>
+                <Typography variant='body2' color='text.secondary' mb={2}>
+                  {blog.source.name} {blog.publishedAt}
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  {blog.description}
+                </Typography>
+                <CardActions>
+                  <Button
+                    href={blog.url}
+                    color={"primary"}
+                    fullWidth
+                    sx={{ mt: 3, textTransform: "initial" }}
+                    variant='outlined'
+                    textTransform='upppercase'
+                  >
+                    CONTINUE READ
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
 
-        {blogs?.totalArticles === 0 && <p>No News found</p>}
-      </div>
-    </div>
+        {blogs?.totalArticles === 0 && (
+          <Typography color='text.primary' variant='body1'>
+            No News found
+          </Typography>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
